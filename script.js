6800c1100fc91cfc9eaaa77644c915ba3debda2d
umernazirbhat <121682568+umernazirbhat@users.noreply.github.com>
2024-04-16 20:33:56 +0530

    adding files to git

---
+++ script.js
@@ -0,0 +1,18 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+  display.value += input;
+  console.log(value);
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
